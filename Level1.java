4f04141ac4359e6dec20cb00589d6c787fb1453e
nicholasarinduque <89489431+nicholasarinduque@users.noreply.github.com>
2021-09-21 15:38:59 -0700

    SamActivity3
    first half is done, I still need to work on it

--- Level1.java
+++ Level1.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class BrickWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Nicholas Arinduque
+ * @version 9/16/2021
  */
 public class Level1 extends World
 {
@@ -16,5 +16,40 @@ public class Level1 extends World
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1, false); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
+            Door.class, HUD.class);
+        Door door = new Door();
+        addObject(door,1174,44);
+        Player player = new Player();
+        addObject(player,96,750);
+        Gem gem = new Gem();
+        addObject(gem, 890, 488);
+        addObject(new Floor(), 600, 800);
+        addObject(new BrickWall(), 380, 500);
+        addObject(new BrickWall(), 960, 100);
+        addObject(new SmBrickWall(), 1120, 600);
+        addObject(new SmBrickWall(), 880, 600);
+        addObject(new SmBrickWall(), 420, 160);
+        addObject(new SmBrickWall(), 1000, 200);
+        addObject(new SmBrickWall(), 220, 280);
+        addObject(new TrapDoor(), 60, 400); 
+        addObject(new TrapDoor(), 465, 115); 
+        addObject(new Bomb(), 1050, 765);
+        addObject(new Gem(), 975, 160);
+        addObject(new Gem(), 1030, 160);
+
+      
+        
+        
+        
     }
 }
